e5d0f45382f75b1c461381295e81665fc5e5032e
Igor <iwebwork.adm@gmail.com>
2021-07-22 00:05:36 -0300

    Ajustes no arquivo App.js

--- App.js
+++ App.js
@@ -7,15 +7,29 @@
  */
 
 import React, {Component} from 'react'
-import {View,StyleSheet} from 'react-native'
+import {View,StyleSheet, Text} from 'react-native'
 
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
 class App extends Component {
 	render() {
+
+		const comments = [
+			{
+				nickname:'etese',
+				comment:'teste 1'
+			},
+			{
+				nickname:'etese',
+				comment:'teste 1'
+			}
+		]
+
 		return (
 			<View style={[styles.container]}>
 				<Header/>	
+				<Post image={require('./assets/imgs/fence.jpg')} comments={comments}/>
 			</View>
 		)
 	}
