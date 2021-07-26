d378080d9e1befdd78efe42b85541f8e23039c16
Igor <iwebwork.adm@gmail.com>
2021-07-26 20:25:47 -0300

    Reducer integrado ao projeto principal

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-
+import React from 'react'
+import {Provider} from 'react-redux'
 import {AppRegistry} from 'react-native';
 import Navigation from './src/navigation/Navigator'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Navigation);
+import storeConfig from './src/store/storeConfig'
+
+const store = storeConfig()
+const Redux = () => (
+    <Provider store={store}>
+        <Navigation/>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Redux);
