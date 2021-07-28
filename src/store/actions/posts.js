1d7d96e0141d58bfefff4cc7fa429f1ed6de83e0
Igor <iwebwork.adm@gmail.com>
2021-07-28 20:06:54 -0300

    Adicionando post no firebase

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -1,10 +1,16 @@
 import { ADD_POST, ADD_COMMENT } from "./actionTypes"
+import axios from 'axios'
 
 export const addPost = post => {
-    return {
-        type: ADD_POST,
-        payload: post
+    return dispatch => {
+        axios.post('/posts/'+post.nickname+'.json', {...post})
+            .catch(err => console.error(err))
+            .then(res => console.log(post))
     }
+    // return {
+    //     type: ADD_POST,
+    //     payload: post
+    // }
 }
 
 export const addComment = payload => {
