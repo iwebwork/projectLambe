02eb6e7bc6f9404f887f867e493f366c02e499f3
Igor <iwebwork.adm@gmail.com>
2021-07-28 23:48:27 -0300

    Salvando o post com a imagem finalizado

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
 const functions = require("firebase-functions");
+const cors = require("cors")({origin: true});
+const fs = require("fs");
+const uuid = require("uuid-v4");
+const {Storage} = require("@google-cloud/storage")
+const storage = new Storage({
+    projectId: 'cod3rlambe',
+    keyFilename: 'cod3rlambe.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        try {
+            fs.writeFileSync('/tmp/imageToSave.jpg', request.body.image, 'base64'); 
+
+            const bucket = storage.bucket('cod3rlambe.appspot.com')
+            const id = uuid();
+            bucket.upload('/tmp/imageToSave.jpg', {
+                uploadType: 'media',
+                destination: `images/posts/${id}.jpg`,
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id
+                    }
+                }
+            },
+            (error, file) => {
+                if(error) {
+                    console.log(error);
+                    return response.status(500).json({error});
+                }else{
+                    const fileName = encodeURIComponent(file.name);
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/' + 
+                        bucket.name + '/o/' + fileName + '?alt=media&token=' + id;
+                    return response.status(201).json({imageUrl: imageUrl});
+                }
+            });
+        } catch (error) {
+            console.log(error)
+            return response.status(500).json({error})
+        }
+    })
 });
--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -3,9 +3,21 @@ import axios from 'axios'
 
 export const addPost = post => {
     return dispatch => {
-        axios.post('/posts/'+post.nickname+'.json', {...post})
-            .catch(err => console.error(err))
-            .then(res => console.log(post))
+        axios({
+            url: 'uploadImage',
+            baseURL: 'https://us-central1-cod3rlambe.cloudfunctions.net',
+            method: 'POST',
+            data: {
+                image: post.image.base64
+            }
+        })
+        .catch(err => console.error(err))
+        .then( res => {
+            post.image = res.data.imageUrl
+            axios.post('/posts/'+post.nickname+'.json', {...post})
+                .catch(err => console.error(err))
+                .then(res => console.log(res.data))
+        })
     }
     // return {
     //     type: ADD_POST,
